55c14b7ac748bab52963a25130380c2fde87e742
TareqKhalili <tareq.kha99@gmail.com>
2021-06-22 20:09:19 +0300

    fixed ports

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -25,5 +25,5 @@ app.get('/:word', (req, res) => {
 
 
 
-app.listen(PORT || process.env.PORT)
+app.listen(PORT)
 
